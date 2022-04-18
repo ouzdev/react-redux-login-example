a24172733da760ad499fe70ba71205f43e05701c
ouzdev <oguzcangencc@hotmail.com>
2022-04-18 21:41:00 +0300

    Coded dark mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { useSelector,useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
 import Login from './components/login';
 import Logout from './components/logout';
 import { selectUser } from './features/loginSlice';
 import { ThemeProvider } from "@mui/material/styles";
-import {
-  FormGroup,
-  FormControlLabel,
-} from "@mui/material";
+import Typography from '@mui/material/Typography';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import IconButton from '@mui/material/IconButton';
@@ -16,7 +13,7 @@ import IconButton from '@mui/material/IconButton';
 import { darkTheme, lightTheme } from "./theme";
 import { toggleTheme } from "../src/features/themeSlice";
 function App() {
-  const user  =useSelector(selectUser)
+  const user = useSelector(selectUser)
   const theme = useSelector((state) => state.theme);
   const dispatch = useDispatch();
   const ToggleSwitch = () => {
@@ -28,27 +25,24 @@ function App() {
           right: "10px",
         }}
       >
-        <FormGroup>
-          <FormControlLabel
-            control={
-              <IconButton sx={{ ml: 1 }} onClick={dispatch(toggleTheme)} color="inherit">
-              {theme.darkTheme === 'light' ? <Brightness7Icon /> : <Brightness4Icon />}
-            </IconButton>
-            }
-            label="Toggle Theme"
-          />
-        </FormGroup>
-      </div>
-    );
+          <Typography component="overline">
+          {theme.darkTheme === false ? 'Light':'Dark'} Mode
+          </Typography>
+        <IconButton sx={{ ml: 1 }} onClick={() => dispatch(toggleTheme())} color="inherit">
+          {theme.darkTheme === false ? <Brightness4Icon />:<Brightness7Icon />}
+        </IconButton>
+
+        </div>
+      );
   };
 
   return (
     <ThemeProvider theme={theme.darkTheme ? darkTheme : lightTheme}>
 
-        <ToggleSwitch />
+      <ToggleSwitch />
 
       {user ? <Logout /> : <Login />}
-          </ThemeProvider>
+    </ThemeProvider>
 
   );
 }
